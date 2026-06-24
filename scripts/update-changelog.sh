0a33f721ed902041c0496d61f56ef7b4ffba7b47
ppip <ppip@duck.com>
2026-06-24 09:42:17 +0800

    fix release ci

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # scripts/update-changelog.sh
 
-NEW_VERSION=$(node -p "require('./package.json').version")
+NEW_VERSION="$1"
 CURRENT_DATE=$(date +%Y-%m-%d)
+
+if [ -z "$NEW_VERSION" ]; then
+  NEW_VERSION=$(node -p "require('./package.json').version")
+fi
 
 sed -i "s/^## \[Unreleased\]/## [$NEW_VERSION] - $CURRENT_DATE/" CHANGELOG.md
